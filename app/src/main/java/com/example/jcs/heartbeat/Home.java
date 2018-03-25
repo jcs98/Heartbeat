f754acb8a01c5ab06c3188f6f201930f4c769ce6
Jainam Shah <jainamcshah98@gmail.com>
2018-03-25 21:53:36 +0530

    Added code to ask for camera permission if not already granted

--- app/src/main/java/com/example/jcs/heartbeat/Home.java
+++ app/src/main/java/com/example/jcs/heartbeat/Home.java
@@ -1,7 +1,12 @@
 package com.example.jcs.heartbeat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
@@ -23,6 +28,8 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        setUpPermissions();
 
         ageEditText = (EditText) findViewById(R.id.et_age);
         rhrLinearLayout = (LinearLayout) findViewById(R.id.ll_restingHeartRate);
@@ -81,6 +88,37 @@ public class Home extends AppCompatActivity {
     public void getRestingHeartRate(){
         Intent intentToGetRHR = new Intent(Home.this, MainActivity.class);
         startActivityForResult(intentToGetRHR, 1);
+    }
+
+    private void setUpPermissions() {
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                String[] permissionsWeNeed = new String[]{Manifest.permission.CAMERA};
+                requestPermissions(permissionsWeNeed, 88);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 88: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // The permission was granted!
+                    //set up whatever required the permissions
+
+                } else {
+                    Toast.makeText(this, "Permission for camera not granted. HeartBeat Monitor can't run.", Toast.LENGTH_LONG).show();
+                    finish();
+                    // The permission was denied, so we can show a message why we can't run the app
+                    // and then close the app.
+                }
+            }
+            // Other permissions could go down here
+
+        }
     }
 
     @Override
